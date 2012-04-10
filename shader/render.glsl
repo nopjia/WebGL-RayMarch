350feda8f8c4fb1ad645ad9dd0b0f6b167cb856b
Nop Jiarathanakul <prutsdomj@gmail.com>
2012-04-10 03:14:48 -0400

    cleaned up unused commented shader code

--- shader/render.glsl
+++ shader/render.glsl
@@ -87,40 +87,6 @@ float opI( float d1, float d2 )
   return max(d1,d2);
 }
 
-vec4 map( in vec3 p ) {
-  float d = sdBox(p,vec3(1.0));
-  
-  vec3 testOffset = vec3(3.0, 0.0, 0.0);
-  float d1 = sdBox(p+testOffset, vec3(1.0));
-  d = ( d > d1 ) ? d1 : d;
-  d1 = sdBox(p-testOffset, vec3(1.0));
-  d = ( d > d1 ) ? d1 : d;
-  vec4 res = vec4( d, 1.0, 0.0, 0.0 );
-
-  /*
-  float s = 1.0;
-  for( int m=0; m<3; m++ ) 
-  {
-    vec3 a = mod( p*s, 2.0 )-1.0;
-    s *= 3.0;
-    vec3 r = abs(1.0 - 3.0*abs(a));
-
-    float da = max(r.x,r.y);
-    float db = max(r.y,r.z);
-    float dc = max(r.z,r.x);
-    float c = (min(da,min(db,dc))-1.0)/s;
-
-    if( c>d )
-    {
-      d = c;
-      res = vec4( d, 0.2*da*db*dc, (1.0+float(m))/4.0, 0.0 );
-    }
-  }
-  */
-
-  return res;
-}
-
 float getDist(in vec3 p) {
   // wrapping xz plane
   //p.x = mod(p.x,4.0)-2.0;
@@ -168,23 +134,6 @@ vec3 getNormal(in vec3 pos) {
   nor.z = getDist(pos+eps.yyx) - getDist(pos-eps.yyx);
   return normalize(nor);
 }
-
-/*
-vec4 intersect( in vec3 ro, in vec3 rd ) {
-  float t = 0.0;
-  vec4 res = vec4(-1.0);
-  for(int i=0;i<64;i++)
-  {
-    vec4 h = map(ro + rd*t);
-    if( h.x<0.002 )
-    {
-      if( res.x<0.0 ) res = vec4(t,h.yzw);
-    }
-    t += h;
-  }
-  return res;
-}
-*/
 
 int intersectSteps(in vec3 ro, in vec3 rd) {
   float t = 0.0;
@@ -326,43 +275,4 @@ void main(void) {
   else {
     gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
   }
-  
-  /*
-  // light
-  vec3 light = normalize(vec3(1.0,0.8,-0.6));
-  
-  vec3 col = vec3(0.0);
-  vec4 tmat = intersect(ro,rd);
-  if( tmat.x>0.0 )
-  {
-    vec3 pos = ro + tmat.x*rd;
-    vec3 nor = getNormal(pos);
-
-    float dif1 = max(0.4 + 0.6*dot(nor,light),0.0);
-    float dif2 = max(0.4 + 0.6*dot(nor,vec3(-light.x,light.y,-light.z)),0.0);
-    
-    // shadow
-    float ldis = 4.0;
-    vec4 shadow = intersect( pos + light*ldis, -light );
-    if( shadow.x>0.0 && shadow.x<(ldis-0.01) ) dif1=0.0;
-    
-    float ao = tmat.y;
-    col  = 1.0*ao*vec3(0.2,0.2,0.2);
-    col += 2.0*(0.5+0.5*ao)*dif1*vec3(1.0,0.97,0.85);
-    col += 0.2*(0.5+0.5*ao)*dif2*vec3(1.0,0.97,0.85);
-    col += 1.0*(0.5+0.5*ao)*(0.5+0.5*nor.y)*vec3(0.1,0.15,0.2);
-    
-    // gamma lighting
-    col = col*0.5+0.5*sqrt(col)*1.2;
-
-    vec3 matcol = vec3(
-      0.6+0.4*cos(5.0+6.2831*tmat.z),
-      0.6+0.4*cos(5.4+6.2831*tmat.z),
-      0.6+0.4*cos(5.7+6.2831*tmat.z) );
-    col *= matcol;
-    col *= 1.5*exp(-0.5*tmat.x);
-  }
-  
-  gl_FragColor = vec4(col,1.0);
-  */
 }
